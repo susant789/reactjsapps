947821c20ca150c2d1638896597abdb89a44cce7
susant789 <susantdasari@gmail.com>
2021-05-27 09:44:40 +0530

    todo_app with firebase logic completed

--- todo_app/src/Components/Todolist.js
+++ todo_app/src/Components/Todolist.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Button, List,ListItem,ListItemText } from '@material-ui/core';
 import '../Todolist.css'
 import db from '../firebase';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 
 
 function Todolist(props) {
@@ -10,9 +11,9 @@ function Todolist(props) {
             <ListItem className="todo_list">
                 <ListItemText primary="Todo..." secondary={props.todo.todo} />
             </ListItem>
-            <Button onClick={e=>{
+            <DeleteForeverIcon onClick={e=>{
                 db.collection('todoapp').doc(props.todo.id).delete()
-            }}>Delete</Button>
+            }}/>
         </List>
     )
 }
